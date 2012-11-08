ebba1162325a0158393b21a44981365f08f8136e
Mikhail <mklimonov@gmail.com>
2012-11-08 18:52:48 +0600

    css images

--- sites/all/themes/JBW/template.php
+++ sites/all/themes/JBW/template.php
@@ -18,7 +18,8 @@ function JBW_form_alter(&$form, &$form_state, $form_id) {
     $form['search_block_form']['#size'] = 30; 
     $form['actions']['submit']['#value'] = t('GO!');
     $form['search_block_form']['#class'] = 'search-form';
-    $form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/searchbutton.png');
+    //$form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/searchbutton.png');
+	$form['actions']['submit'] = array('#type' => 'submit');
     $form['search_block_form']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search';}";
     $form['search_block_form']['#attributes']['onfocus'] = "if (this.value == 'Search') {this.value = '';}";
     $form['#attributes']['onsubmit'] = "if(this.search_block_form.value=='Search'){ alert('Please enter a search'); return false; }";
