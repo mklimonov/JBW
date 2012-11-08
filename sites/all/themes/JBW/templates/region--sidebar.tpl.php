fdf4ee91e71e495631308f9acdc35c3a3244d418
Artemnk <shelkov1991@gmail.com>
2012-11-08 11:47:47 +0700

    New project..

---
+++ sites/all/themes/JBW/templates/region--sidebar.tpl.php
@@ -0,0 +1,14 @@
+<div<?php print $attributes; ?>>
+
+<div id="scrollbarY">
+    <div class="scrollbar"><div class="track"><div class="thumb"><div class="end"></div></div></div></div>
+    <div class="viewport">
+        <div class="overview">
+            <div<?php print $content_attributes; ?>>
+				<?php print $content; ?>
+			</div>
+        </div>
+    </div>
+</div>
+
+</div>
