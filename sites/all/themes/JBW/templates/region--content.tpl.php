8a108bd6f9a199a4a36bd20fcad64e2918013598
Artemnk <shelkov1991@gmail.com>
2012-11-12 13:09:21 +0700

    s

--- sites/all/themes/JBW/templates/region--content.tpl.php
+++ sites/all/themes/JBW/templates/region--content.tpl.php
@@ -1,6 +1,10 @@
 <div<?php print $attributes; ?>>
   <div<?php print $content_attributes; ?>>
-  <div style = 'background-color: white;'>
+  
+  <?php if(arg(0) != 'home' && arg(0) != ''): ?>
+	<div style = 'background-color: white;'>
+  <?php endif; ?>
+  
     <a id="main-content"></a>
     <?php print render($title_prefix); ?>
     <?php if ($title): ?>
@@ -13,6 +17,10 @@
     <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
     <?php print $content; ?>
     <?php if ($feed_icons): ?><div class="feed-icon clearfix"><?php print $feed_icons; ?></div><?php endif; ?>
-  </div>
+	
+  <?php if(arg(0) != 'home' && arg(0) != ''): ?>
+	</div>
+  <?php endif; ?>
+  
   </div>
 </div>
