3b2e8035d68297b1424a0044ceb4ad2faf478b33
Mikhail <mklimonov@gmail.com>
2012-11-28 19:47:13 +0600

    fix fly Footer images Font, title

--- sites/all/themes/JBW/templates/region--footer.tpl.php
+++ sites/all/themes/JBW/templates/region--footer.tpl.php
@@ -1,4 +1,5 @@
 <div<?php print $attributes; ?>>
+    <div class="images">
   <?php print $content; ?>
     
 <!--  <div class="images">
@@ -6,9 +7,9 @@
     <img src="<?php //print path_to_theme() .'/images/footer/blackwell.png'?>" />  
   </div>-->
 
-  <div class="images">
-    <img src="<?php print drupal_get_path('theme', 'JBW') .'images/footer/blackwell.png'?>" /> 
-    <img src="<?php print drupal_get_path('theme', 'JBW') .'images/footer/logo.png'?>" />
+  
+ <!--   <img src="<?php //print drupal_get_path('theme', 'JBW') .'images/footer/blackwell.png'?>" /> 
+    <img src="<?php //print drupal_get_path('theme', 'JBW') .'images/footer/logo.png'?>" />-->
  
   </div>
     
