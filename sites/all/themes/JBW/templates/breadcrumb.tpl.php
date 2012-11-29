480f94d6b5e3e157746710cb86dee55534c1865c
Artemnk <shelkov1991@gmail.com>
2012-11-29 12:30:40 +0700

    Commerce views: checkout, shopping cart slider fix - change tpl headersettings reload some widget changes in content-types.

--- sites/all/themes/JBW/templates/breadcrumb.tpl.php
+++ sites/all/themes/JBW/templates/breadcrumb.tpl.php
@@ -1,7 +1,9 @@
 <div id="breadcrumb">
     <?php $breadcrumb = $variables['breadcrumb'];?>
     <?php
-		if(arg(0) != 'blog'){
+    if(arg(0) == 'cart'){
+      print l('Continue shopping', url('events'));
+    }else if(arg(0) != 'blog'){
 			print implode(' > ', $breadcrumb);
 		}
 	?>
